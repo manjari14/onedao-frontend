c8132e93cae85856d91af3358bd3af4a99f50133
Manjari14 <sheoranmanjari@gmail.com>
2025-04-30 17:59:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Login from "./pages/login";
 import Signup from "./pages/signup";
@@ -10,14 +15,19 @@ import OtpVerification from "./pages/OtpVerification";
 import "./App.css";
 
 function App() {
+  const token = localStorage.getItem("token"); // or sessionStorage
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={token ? <Navigate to="/login" replace /> : <Signup />}
+          />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-
           <Route path="/otp" element={<OtpVerification />} />
         </Routes>
       </div>
